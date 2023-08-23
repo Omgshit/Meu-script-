68f905906242bd21d21a315c89175462c529109b
Omgshit <123563422+Omgshit@users.noreply.github.com>
2023-08-22 21:43:08 -0300

    Create MainLoader.lua

---
+++ MainLoader.lua
@@ -0,0 +1,24 @@
+game:GetService("UserInputService").InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.LeftControl then
+        if game:GetService("CoreGui"):FindFirstChild("Omg Hub| "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name) then
+            game:GetService("CoreGui"):FindFirstChild("Omg Hub | "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name).Enabled = not game:GetService("CoreGui"):FindFirstChild("Muimi Hub | "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name).Enabled
+        end
+    end
+end)
+if game.PlaceId == 10598963932 then
+    print ("Cat Piece")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Cat%20piece"))()
+elseif game.PlaceId == 11448431235 then
+    print ("Rock Fruit")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Rock%20Fruit"))()
+elseif game.PlaceId == 9144187696 then
+    print ("A Piece")
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/A%20Piece"))()
+elseif game.PlaceId == 12153639244 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Anime%20Max%20Simulator"))()
+    print ("Anime Max Simulator")
+elseif game.PlaceId == 12988931150 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Merge%20Anime%20Legends%20Simulator"))()
+    else
+        print ("Game not found")
+end
