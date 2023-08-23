eeb236563b6b3046fd71b4990768e11a351267b3
Omgshit <123563422+Omgshit@users.noreply.github.com>
2023-08-23 00:45:16 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -8,14 +8,8 @@ end)
 if game.PlaceId == 10598963932 then
     print ("Cat Piece")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Cat%20piece"))()
-if game.PlaceId == 12364861304 then
-    print ("Second Sea")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Cat%20piece"))()
 elseif game.PlaceId == 11448431235 then
     print ("Rock Fruit")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Rock%20Fruit"))()
-elseif game.PlaceId == 12364861304 then
-    print ("Second Map")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Rock%20Fruit"))()
     elseif game.PlaceId == 9144187696 then
     print ("A Piece")
@@ -26,5 +20,12 @@ elseif game.PlaceId == 12153639244 then
 elseif game.PlaceId == 12988931150 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Merge%20Anime%20Legends%20Simulator"))()
     else
-        print ("Game not found")
+elseif game.PlaceId == 12364861304 then
+    print ("Second Sea")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Cat%20piece"))()
+        elseif game.PlaceId == 12364861304 then
+    print ("Second Map")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Rock%20Fruit"))()
+        
+    print ("Game not found")
 end
