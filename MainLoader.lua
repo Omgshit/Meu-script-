28ef23b4c5319362e7afeaa53daecf42b6000c26
Omgshit <123563422+Omgshit@users.noreply.github.com>
2023-10-28 12:28:11 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -83,7 +83,7 @@ elseif game.PlaceId == 4442272183 then
 elseif game.PlaceId == 7449423635 then
     print ("Sea 3")
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Blox%20fruit"))()
-elseif game.PlaceId == 6938803436 or game.PlaceId == 6990129309 or game.PlaceId == 6990133340 or game.PlaceId == 6990131029 or game.PlaceId == 7338881230 or game.PlaceId == 7274690025 then
+elseif game.PlaceId == 6938803436 or 6990129309 or 6990133340 or 6990131029 or 7338881230 or 7274690025 then
     print ("Anime Dimensions")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Anime%20Dimensions"))()
 else
