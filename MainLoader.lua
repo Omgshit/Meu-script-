e6c4d2e676d5bdf10c82045eceb776688391a0cf
Omgshit <123563422+Omgshit@users.noreply.github.com>
2023-12-04 10:46:26 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -140,6 +140,9 @@ elseif game.PlaceId == 14915220621 then
 elseif game.PlaceId == 15234596844 then
     print ("Blade Ball")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Blade%20BallW"))()
+elseif game.PlaceId == 15185247558 then
+    print ("Blade Ball")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Blade%20BallW"))()
 elseif game.PlaceId == 12398966422 then
     print ("Doors")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/DoorsNotByme"))()
