3ba731f93d19001b7aa50ae96ce6739fb8876f77
Omgshit <123563422+Omgshit@users.noreply.github.com>
2024-09-13 20:50:17 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -30,7 +30,7 @@ elseif game.PlaceId == 10260193230 or game.PlaceId == 11487720177 then
 elseif game.PlaceId == 13601434903 then
     print ("Sorcerer Battlegrounds")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Sorcerer%20Battleground"))()
-elseif game.PlaceId == 18337464872 then
+elseif game.PlaceId == 18337464872 or game.PlaceId == 114252638377752 then
     print ("Weak Legacy")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Weak%20Legacy%202"))()
 elseif game.PlaceId == 9704927237 or game.PlaceId == 12940506339 then
